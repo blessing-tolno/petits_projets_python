6f1af442cfe0c0f075f93f437b3e90b4134bad55
blessing-tolno <barthelemysalomon77@gmail.com>
2022-12-03 23:01:48 +0100

    Travailler avec les f-strings

---
+++ projet1.py
@@ -0,0 +1,15 @@
+#vous allez apprendre comment recevoir une entrée de l'utilisateur, 
+# travailler avec les f-strings, et voir les résultats affichés dans le terminal.
+
+print("ENtrer ces mots comme suit: informatique, échecs, règles, tempérament, fous")
+science = input("Entrer un de vos choix ")
+nom = input("Entrer un de vos choix ") 
+choix3 = input("Entrer un de vos choix ")
+choix4 = input("Entrer un de vos choix ")
+choix5 = input("Entrer un de vos choix ")
+print()
+print(f"Plus il a affaire à l’{science}, plus cela lui semble être comme un jeu d’{nom} : \
+    un petit monde bien clos défini par des {choix3} fabriquées, un monde dans lequel des garçons \
+    prédisposés par un type de {choix4} se laissent prendre et deviennent à moitié {choix5}, \
+    tout comme il est lui-même à demi-fou, de sorte que qu’ils se leurrent en croyant qu’ils jouent à ce jeu, \
+    alors que c’est le jeu qui se joue d’eux.")
